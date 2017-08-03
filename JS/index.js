ccaf5ca80804700e29c2307fed31e9993e120e46
Josh Kim <josh94kim@berkeley.edu>
2017-08-03 11:48:19 -0700

    Added js functionality

--- JS/index.js
+++ JS/index.js
@@ -7,8 +7,8 @@ function getDataFromApi(searchTerm, callback) {
     url: YOUTUBE_SEARCH_URL,
     data: {
       part: 'snippet',
-      key: KEY
-      q: 
+      key: KEY,
+      q: searchTerm,
     },
     dataType: 'json',
     type: 'GET',
@@ -21,16 +21,50 @@ function getDataFromApi(searchTerm, callback) {
 }
 
 function renderResult(result) {
-
+  return `
+    <div>
+      <h2 class="js-result-title">${result.snippet.title}</h2>
+      <a class="js-result-thumbnail" href="https://www.youtube.com/watch?v=${result.id.videoId}"
+        target="_blank" data-toggle="lightbox">
+        <img src="${result.snippet.thumbnails.medium.url}"
+        alt="thumbnail of youtube video" class="js-result-thumbnail">
+      </a>
+      <p>
+        Youtuber: <a class="js-channel-id" href="https://www.youtube.com/channel/${result.snippet.channelId}"
+        target="_blank">${result.snippet.channelTitle}</a>
+      </p>
+    </div>
+  `;
 }
 
-function displayYoutubeData() {
-
+function displayYoutubeSearchData(data) {
+  console.log(data);
+  const results = data.items.map((item, index) => renderResult(item));
+  $('.js-search-results').html(results);
 }
 
 function watchSubmit() {
   $('.js-search-form').submit(event => {
     event.preventDefault();
-
+    const searchTermTarget = $(event.currentTarget).find('.js-query');
+    const searchTerm = searchTermTarget.val();
+    // Clear out the input
+    searchTermTarget.val('');
+    getDataFromApi(searchTerm, displayYoutubeSearchData);
   });
 }
+
+$(watchSubmit);
+
+$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+  event.preventDefault();
+  $(this).ekkoLightbox();
+});
+
+/*
+<a class="js-result-thumbnail" href="https://www.youtube.com/watch?v=${result.id.videoId}"
+  target="_blank" data-toggle="lightbox">
+  <img src="${result.snippet.thumbnails.medium.url}"
+  alt="thumbnail of youtube video" class="js-result-thumbnail">
+</a>
+*/
